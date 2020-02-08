3687bcb2f6718eca00dae711393ddffa3bc9b486
Amithus1 <amlan1304@gmail.com>
2020-02-08 20:05:53 +0530

    08/02 20:05

--- first.R
+++ first.R
@@ -1,2 +1,18 @@
 #first file
-mtcars
+library(VIM)
+data(sleep)
+str(sleep)
+summary(sleep)
+names(sleep)
+is.na(sleep$Dream)
+sum(is.na(sleep$Dream))
+colSums(is.na(sleep))
+rowSums(is.na(sleep))
+complete.cases(sleep)
+sleep[complete.cases(sleep),]
+
+x2= c(5,5,NA,20)
+is.na(x2)
+
+x2[is.na(x2)] = mean (x2 , na.rm = T)
+x2
