56fefcbc98f4953d472c7c71153b9857655871ec
Amithus1 <amlan1304@gmail.com>
2020-02-08 19:55:00 +0530

    08/02 19:54

--- models.R
+++ models.R
@@ -19,3 +19,37 @@ range(women$height)
 #rsquare coefficient of determination
 # more features are added only if variance is added beacuase of it
 residuals(model)
+#checking for assumptions in linear regression through graphs
+
+plot(model)
+#cooks distance s used to find the infuential outliers
+
+predict(model,new = data.frame(height = 77))
+ # find linear cor and cov
+#to model to exist f stat
+#single = multiple r2 , multiple adjusted r2
+
+#mtcars
+
+head(mtcars)
+
+model2 = lm(mpg ~wt + hp + qsec , dat = mtcars)        
+summary(model2)
+
+model3 = lm(mpg ~ wt , data = mtcars)
+summary(model3)
+
+library (olsrr)
+
+fit = lm(mpg ~ disp + hp +wt + qsec , data = mtcars)
+
+k = ols_step_all_possible(fit)
+plot(k)
+
+# load the data
+#find out correlation and missing value
+#form linear model
+#chck for model summary (f stat , r-square,coefficient p value)
+#check for assumption
+#predict and find accuracy
+
---
+++ wordcloud.R
@@ -0,0 +1,16 @@
+#wordcloud
+install.packages('wordcloud2')
+library(wordcloud2)
+df = data.frame(word = c('mdi','iim','imt'),freq = c(20,23,15))
+df
+par(mar=c(0,0,0,0))
+wordcloud2(df)
+
+head(demoFreq)
+dim(demoFreq)
+par(mar=c(0,0,0,0))
+
+wordcloud2(demoFreq)
+
+par(mar=c(0,0,0,0))
+wordcloud2(demoFreq,size 2)
